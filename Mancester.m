17bf452b1ab79027f6dd6f2cdfbe87a0d486038c
Chandan <forumguru2015@gmail.com>
2016-02-19 19:48:38 +0530

    chandan

---
+++ Mancester.m
@@ -0,0 +1,35 @@
+clc;
+x = input('Enter the binary sequence');
+if( max(x)>1 || min(x)<0)
+    disp('error')
+    return
+end
+N = length(x);
+n=0.001:0.001:N;
+line=zeros(1,1000*N);
+subplot(211)
+plot(n,(square(2*pi*1*n,50)/2)+0.5,'LineWidth',3);
+xlabel('time')
+ylabel('amplitude')
+title('clock')
+if x(1)== 1
+    line(1,1:500) = 1;
+    line(1,500:1000) = 0;        
+else
+    line(1,1:500) = 0;
+    line(1,500:1000) = 1; 
+end
+for l=1:1:N-1
+    if x(l+1)== 1
+        line(1,(l*1000):(l*1000+500)) = 1;
+        line(1,(l*1000+500):(l*1000+1000)) = 0;        
+    else
+        line(1,(l*1000):(l*1000+500)) = 0;
+        line(1,(l*1000+500):(l*1000+1000)) = 1; 
+    end
+end
+subplot(212)
+plot(n,line,'LineWidth',3)
+xlabel('time')
+ylabel('amplitude')
+title('Manchester coding waveform')
